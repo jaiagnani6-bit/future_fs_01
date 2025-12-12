6898ee6d7e4cb514f8aaf25319dc5443e05ecbce
jaiagnani6-bit <jaiagnani6@gmail.com>
2025-12-13 01:23:44 +0530

    Started a new project

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,74 @@
+
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Highly motivated Computer Science student with a
+            strong foundation in data structures and algorithms,
+            seeking to leverage hands-on experience in Python and
+            JavaScript to develop and test scalable end-user
+            applications. Proven ability to collaborate on team
+            projects and quickly master new technologies to solve
+            complex engineering challenges.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "High Schooling",
+      content: (
+        <>
+          <p>
+            My time at Jaishree Periwal High School sparked my initial interest in problem-solving and logical reasoning. 
+            Graduating in 2024, I transitioned immediately into computer science with a drive to apply these foundational skills to real-world software challenges.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2028 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            As a Computer Science student, I am dedicated to bridging the gap between theory and application. 
+            My academic journey is defined by: * Core Competencies: Data Structures, Algorithms, and System Design. * Technical Stack: Proficient in Java, Python, and JavaScript. * Project Focus: Developing collaborative software solutions and integrating APIs.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2025 Full Stack Developer Intern at The Devlopers Arena ",
+      content: (
+        <>
+          <p>
+           As a Full Stack Developer Intern at Developers Arena, I was responsible for the end-to-end development of several key projects within a self-paced, results-driven structure. 
+           I architected and implemented features for both the client and server side, leveraging React.js for a modern, component-driven frontend and Node.js/Express for efficient backend services and API design. 
+           This role significantly sharpened my skills in taking ownership of the full stack and managing complex, independent development tasks.
+          </p>
+        </>
+      ),
+    },
+
+    {
+      title: "2025 Full Stack Devloper Intern at Future Interns ",
+      content: (
+        <>
+          <p>
+           As a Full Stack Developer Intern at Future Interns, I was responsible for the end-to-end development of several key projects within a self-paced, results-driven structure. 
+           I architected and implemented features for both the client and server side, leveraging React.js for a modern, component-driven frontend and Node.js/Express for efficient backend services and API design. 
+           This role significantly sharpened my skills in taking ownership of the full stack and managing complex, independent development tasks.
+          </p>
+        </>
+      ),
+    },
+  ],
+ };
+
+export default subheadingsData;
---
+++ src/skills/skillsData.js
@@ -0,0 +1,69 @@
+
+const skills = {
+  1: [
+    {
+      title: "HTML",
+      level: 6,
+    },
+    {
+      title: "CSS",
+      level: 5,
+    },
+    {
+      title: "SASS",
+      level: 2,
+    },
+    {
+      title: "JavaScript",
+      level: 5,
+    },
+    {
+      title: "TypeScript",
+      level: 4,
+    },
+    {
+      title: "Tailwind CSS",
+      level: 1,
+    },
+    {
+      title: "React JS",
+      level: 3,
+    },
+  ],
+  2: [
+    {
+      title: "Spring ",
+      level: 3,
+    },
+    {
+      title: "Node.js",
+      level: 4,
+    },
+    {
+      title: "Framework",
+      level: 2,
+    },
+    {
+      title: "Git",
+      level: 4,
+    },
+    {
+      title: "PHP",
+      level: 1,
+    },
+    {
+      title: "SQL",
+      level: 3,
+    },
+    {
+      title: "Express.js",
+      level: 3,
+    },
+    {
+      title: "Java",
+      level: 6,
+    },
+  ],
+};
+
+export default skills;
